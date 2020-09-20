242802278cad5b4d8af032c3d4aed3c5fcdbccbb
Larionov-Yurii <69587209+Larionov-Yurii@users.noreply.github.com>
2020-09-21 00:18:41 +0300

    Display all main comments on page

--- check info/allmaincomments.php
+++ check info/allmaincomments.php
@@ -6,3 +6,4 @@
     $showcomn = $showcomn->fetchAll();
 
     $certainuser = $Gcom->query("SELECT name, comment, DATE_FORMAT( comments.date, '%Y-%m-%d' ) AS date FROM `comments` INNER JOIN `new users` ON comments.userID = `new users`.id");
+
