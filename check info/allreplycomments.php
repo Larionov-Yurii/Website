e9c05dc862cf664f5f358182352915d1953a8396
Larionov-Yurii <69587209+Larionov-Yurii@users.noreply.github.com>
2020-09-21 00:19:05 +0300

    Display all reply comments

--- check info/allreplycomments.php
+++ check info/allreplycomments.php
@@ -6,4 +6,3 @@
     $showreplycom = $showreplycom->fetchAll();
 
     $certaincom = $Gcom->query("SELECT `replycom`.id, name, comment, DATE_FORMAT( replycom.date, '%Y-%m-%d' ) AS date FROM `replycom` INNER JOIN `new users` ON replycom.userID = `new users`.id ORDER BY `replycom`.id DESC");
-
